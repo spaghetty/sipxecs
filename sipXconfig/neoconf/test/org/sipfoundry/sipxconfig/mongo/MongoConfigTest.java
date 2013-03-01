SIPfoundry/legacy-sipxecs

Squashed commit of the following:
commit b1b7543d98d562a8a3d329bd85acca7e28bb4af3
Author: Alex Mateescu <amateescu@ezuce.com>
Date:   Mon Dec 3 17:23:53 2012 +0200

    Align with Openfire changes

commit d222140859f040dc2a08a088c88a05614b7d3ba4
Author: Alex Mateescu <amateescu@ezuce.com>
Date:   Mon Nov 19 14:00:39 2012 +0200

    Some interfaces have changed packages

sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/mongo/MongoConfigTest.java
@@ -28,28 +28,26 @@ import org.junit.Test;
 import org.sipfoundry.sipxconfig.commserver.Location;
 
 public class MongoConfigTest {
-    private MongoConfig m_config;
     private List<Location> m_single;
     private List<Location> m_multi;
-    
+
     @Before
     public void setUp() {
-        m_config = new MongoConfig();
         m_multi = Arrays.asList(new Location("one"), new Location("two"));
         m_multi.get(0).setAddress("1.1.1.1");
         m_multi.get(1).setAddress("1.1.1.2");
         m_single = Collections.singletonList(m_multi.get(0));
     }
-    
+
     @Test
     public void getConnectionString() {
-        assertEquals("sipxecs/one:1", m_config.getConnectionString(m_single, 1));
-        assertEquals("sipxecs/one:1,two:1", m_config.getConnectionString(m_multi, 1));
+        assertEquals("sipxecs/one:1", MongoConfig.getConnectionString(m_single, 1));
+        assertEquals("sipxecs/one:1,two:1", MongoConfig.getConnectionString(m_multi, 1));
     }
 
     @Test
     public void getConnectionUrl() {
-        assertEquals("mongodb://one:1/?slaveOk=true", m_config.getConnectionUrl(m_single, 1));
-        assertEquals("mongodb://one:1,two:1/?slaveOk=true", m_config.getConnectionUrl(m_multi, 1));
+        assertEquals("mongodb://one:1/?readPreference=nearest", MongoConfig.getConnectionUrl(m_single, 1));
+        assertEquals("mongodb://one:1,two:1/?readPreference=nearest", MongoConfig.getConnectionUrl(m_multi, 1));
     }
 }

spaghetty/sipxecs

Conflicts:
	sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/mongo/MongoConfigTest.java

sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/mongo/MongoConfigTest.java
@@ -55,7 +55,7 @@ public class MongoConfigTest {
 
     @Test
     public void getConnectionUrl() {
-        assertEquals("mongodb://one:1/?slaveOk=true", m_config.getConnectionUrl(m_single, 1));
-        assertEquals("mongodb://one:1,two:1/?slaveOk=true", m_config.getConnectionUrl(m_multi, 1));
+        assertEquals("mongodb://one:1/?readPreference=nearest", m_config.getConnectionUrl(m_single, 1));
+        assertEquals("mongodb://one:1,two:1/?readPreference=nearest", m_config.getConnectionUrl(m_multi, 1));
     }
 }