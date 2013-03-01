22b3c99177e32bb0abe41cdf8be7520262ffee8c
laurentiu <laurentiu@ezuce.com>
2013-03-01 10:48:04 +0200

    PT-45056929 - Add back support agent dial strings in openacd - delete all existing agent dial strings and re-create only login and logout with the properly values - doesn't let the user delete or rename default agent dial strings (login and logout)

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/openacd/OpenAcdCommand.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/openacd/OpenAcdCommand.java
@@ -26,12 +26,10 @@ public class OpenAcdCommand extends OpenAcdExtension {
     public static List<FreeswitchAction> getDefaultActions(Location location) {
         List<FreeswitchAction> actions = new LinkedList<FreeswitchAction>();
         actions.add(createAction(FreeswitchAction.PredefinedAction.erlang_sendmsg.toString(),
-                "agent_dialplan_listener  " + "openacd@" + location.getFqdn()
-                        + " agent_login ${sip_from_user} pstn ${sip_from_uri}"));
+                "oacd_dialplan_listener  " + "openacd@" + location.getFqdn() + " agent_login ${sip_from_user}"));
         actions.add(createAction(FreeswitchAction.PredefinedAction.answer.toString(), null));
         actions.add(createAction(FreeswitchAction.PredefinedAction.sleep.toString(), "2000"));
         actions.add(createAction(FreeswitchAction.PredefinedAction.hangup.toString(), "NORMAL_CLEARING"));
         return actions;
     }
-
 }
