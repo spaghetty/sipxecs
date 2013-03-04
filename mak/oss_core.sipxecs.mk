1376802b7eaa8e8059146f8254e4fcafed1bf9e6
Douglas Hubler <dhubler@ezuce.com>
2013-03-04 07:28:22 -0500

    update oss_core

--- mak/oss_core.sipxecs.mk
+++ mak/oss_core.sipxecs.mk
@@ -1,7 +1,7 @@
-oss_core_VER = 1.6.0
+oss_core_VER = 2.0.0
 oss_core_REV = $(shell cd $(SRC)/$(PROJ); ../config/revision-gen $(oss_core_VER))
 oss_core_TAR = $(PROJ)/oss_core-$(oss_core_VER).tar.gz
 oss_core_SRPM = oss_core-$(oss_core_VER)-$(oss_core_REV).src.rpm
 oss_core_OPTIONS = --disable-dep-check
 
-oss_core.dist : oss_core.configure oss_core.autoreconf oss_core.dist_;
+oss_core.dist : oss_core.autoreconf oss_core.configure oss_core.dist_;
